19c8ff8ed70193927c056aaca728474b58511527
Bill Healey <bill@presto.com>
2024-12-26 21:59:52 -0800

    Added serial and http interfaces for RoArm-M2-S, added 3d spacial tracking via iPhone for teleoperation

--- tracker.py
+++ tracker.py
@@ -5,24 +5,28 @@ import numpy as np
 
 class MobileIOARTracker:
     def __init__(self):
-        lookup = hebi.Lookup()
-        time.sleep(.25)
-        device = None
-        for entry in lookup.entrylist:
-            print(entry)
-            device = entry
-        if not device:
-            raise Exception("No Device found, start Mobile IO app on iphone")
-        self.group = lookup.get_group_from_names([device.family], [device.name])
-        self.fbk = hebi.GroupFeedback(self.group.size)
         self._initial_position = None
-        self.reset_initial_position()
+        device = None
+
+        while device is None:
+            lookup = hebi.Lookup()
+            time.sleep(0.25)
+            for device in lookup.entrylist:
+                break
+            if device is None:
+                print("No Device found, start Mobile IO app on iPhone")
+                time.sleep(1)
+
+        self.group = lookup.get_group_from_names([device.family], [device.name])
+        self.group.feedback_frequency = 200  # 200Hz, max is likely 1kHz linux and 640Hz Win7
+        self.fbk = hebi.GroupFeedback(self.group.size)
 
     def reset_initial_position(self):
-        self._initial_position = np.ndarray.copy(self._get_pos())
-        print("Initial position reset successfully.")
+        self._initial_position = None
 
-    def _get_pos(self):
+    def _get_feedback(self):
+        axes = {}
+        buttons = {}
         self.fbk = self.group.get_next_feedback(reuse_fbk=self.fbk)
         if self.fbk is None:
             print("Could not get feedback")
@@ -30,35 +34,43 @@ class MobileIOARTracker:
         # reorder w,x,y,z to x,y,z,w
         orient = [*orient[1:], orient[0]]
         pos = self.fbk[0].ar_position
+        for i in range(1, 9):
+            io_a = self.fbk[0].io.a
+            io_b = self.fbk[0].io.b
+            axes[i] = io_a.get_float(i)
+            buttons[i] = io_b.get_int(i)
         #r = R.from_quat(orient)
         #rot_mat = r.as_matrix()
         #rot_mat = np.eye(3)
+        if self._initial_position is None:
+            self._initial_position = np.ndarray.copy(pos)
 
-        return pos
-
-    def get_relative_position(self):
-        return self._get_pos() - self._initial_position
+        return {
+            'pos': pos,
+            'relative_pos': pos - self._initial_position,
+            'axes': axes,
+            'buttons': buttons,
+        }
 
     def continuous_tracking(self, callback=None, duration=None):
         start_time = time.time()
         try:
             while duration is None or time.time() - start_time < duration:
-                relative_pos = self.get_relative_position()
+                feedback = self._get_feedback()
                 if callback:
-                    callback(relative_pos)
-                time.sleep(0.05)
+                    callback(feedback)
         except KeyboardInterrupt:
             print("Tracking stopped by user.")
 
 
-def print_position(pos):
+def print_position(feedback):
+    pos = feedback['pos']
     print(f"Relative Position: x={pos[0]:.3f}, y={pos[1]:.3f}, z={pos[2]:.3f} meters")
 
 
 def main():
     try:
         tracker = MobileIOARTracker()
-        tracker.reset_initial_position()
         tracker.continuous_tracking(callback=print_position, duration=60)
     except Exception as e:
         print(f"Error: {e}")
